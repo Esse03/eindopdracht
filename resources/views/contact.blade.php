27ab58e8c4589afcd5bc807f32af5eb30d1b505b
Esse03 <essetijdelijk@gmail.com>
2024-10-15 10:06:31 +0200

    added changes include: begin assignment home, database features(animals table), 1st model base

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<h1>Contact</h1>
+<section>
+    <div>
+        <a href="{{route('contact', ['company' => 'matennaaiers'])}}">Matennaaiers</a>
+        <a href="{{route('contact', ['company' => 'nietpersematennaaiersmaareriszekersexuelespanning'])}}">Niet per se matennaaiers maar er is zeker sexuele spanning</a>
+        <a href="{{route('contact', ['company' => 'matenliefdesbedrijvers'])}}">Matenliefdesbedrijvers</a>
+
+    </div>
+</section>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>The Weird made up animal collection</h1>
+</x-layout>
